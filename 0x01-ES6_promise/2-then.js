72cac5a354a2088b1dd595b3b13f7679ebccefe4
Reid-T-W <rediettadesse100@gmail.com>
2023-01-24 21:32:01 +0300

    logging moved to then block only

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -5,10 +5,8 @@ export default function handleResponseFromAPI(promise) {
         status: 200,
         body: 'success',
       };
+      console.log('Got a response from the API');
       return obj;
     })
-    .catch(() => { Error(); })
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
+    .catch(() => { Error(); });
 }
