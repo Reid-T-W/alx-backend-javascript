27332f2a0907f6607497a7d8c402339928004a86
Reid-T-W <rediettadesse100@gmail.com>
2023-01-25 14:08:04 +0300

    handle settled promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,16 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
   return Promise.allSettled(promises)
-    .then(() => (
+    .then((data) => (
       {
-        status: 'status_of_the_promise',
-        value: 'value or error returned by the Promise',
+        status: 'fulfilled',
+        value: data,
+      }
+    ))
+    .catch((error) => (
+      {
+        status: 'rejected',
+        value: error,
       }
     ));
 }
