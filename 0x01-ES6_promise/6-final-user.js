25932b316a350813f788f608e8531c6b88694704
Reid-T-W <rediettadesse100@gmail.com>
2023-01-25 15:03:15 +0300

    status extracted from returned object

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,16 +4,12 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
   return Promise.allSettled(promises)
-    .then((data) => (
-      {
-        status: 'fulfilled',
-        value: data,
-      }
-    ))
-    .catch((error) => (
-      {
-        status: 'rejected',
-        value: error,
-      }
+    .then((results) => (
+      results.forEach((result) => (
+        {
+          status: result.status,
+          value: result,
+        }
+      ))
     ));
 }
