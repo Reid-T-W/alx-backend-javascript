5db8f358505cec036adb293b056aa2a46bedd494
Reid-T-W <rediettadesse100@gmail.com>
2023-01-25 16:19:20 +0300

    Returns an array of values

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,13 +3,12 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  const all = [];
   return Promise.allSettled(promises)
-    .then((results) => (
-      results.forEach((result) => (
-        {
-          status: result.status,
-          value: result,
-        }
-      ))
-    ));
+    .then((results) => {
+      results.forEach((result) => {
+        all.push({ status: result.status, value: result });
+      });
+      return all;
+    });
 }
