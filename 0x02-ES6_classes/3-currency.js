d8de98558ed1f4c009c5291dfcd0fc2d1f6fd4fd
Reid-T-W <rediettadesse100@gmail.com>
2023-01-26 00:12:42 +0300

    getters and setters added

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,18 +1,26 @@
 export default class Currency {
   constructor(code, name) {
-    if (typeof code === 'string') {
-      this._code = code;
-    } else {
-      throw new Error('TypeError: Code must be a string');
-    }
-    if (typeof name === 'string') {
-      this._name = name;
-    } else {
-      throw new Error('TypeError: Name must be a string');
-    }
+    this._code = code;
+    this._name = name;
   }
 
   displayFullCurrency() {
     return (`${this._name} (${this._code})`);
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
   }
 }
