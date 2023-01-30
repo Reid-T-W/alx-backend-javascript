7ae37fc00ba14daa79ae8e8f02a901e8e1d69f32
Reid-T-W <rediettadesse100@gmail.com>
2023-01-31 00:54:08 +0300

    object with additional information returned

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -4,7 +4,10 @@ export default function createInt8TypedArray(length, position, value) {
   }
   const buffer = new ArrayBuffer(length);
   const int8View = new Int8Array(buffer);
-  // int8View.set(value, position);
   int8View[position] = value;
-  return buffer;
+  return {
+    byteLength: length,
+    byteOffset: 0,
+    buffer,
+  };
 }
