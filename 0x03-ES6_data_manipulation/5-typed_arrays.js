054b28d40c13163bcf6dec59d6af4e419efa3ff6
Reid-T-W <rediettadesse100@gmail.com>
2023-01-31 01:13:30 +0300

    error thrown in try-catch statement

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,14 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position >= length) {
-    return Error('Position outside range');
-  }
+  // if (position >= length) {
+  //  return Error('Position outside range');
+  // }
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer, 0);
-
-  view.setInt8(position, value);
+  try {
+    view.setInt8(position, value);
+  } catch (e) {
+    throw (Error('Position outside range'));
+  }
   return view;
 
   // const int8View = new Int8Array(buffer);
