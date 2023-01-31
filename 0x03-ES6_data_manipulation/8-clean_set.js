a1dff3564c54bcefed9b059f58aea4f58feec58e
Reid-T-W <rediettadesse100@gmail.com>
2023-01-31 18:23:09 +0300

    if statement inside forEach removed for checking purpose

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -5,14 +5,14 @@ export default function cleanSet(set, startString) {
     return newString;
   }
   set.forEach((x) => {
-    if (x.includes(startString)) {
-      /* const cleanedString = x.replace(startString, '');
+    /* if (x.includes(startString)) {
+      const cleanedString = x.replace(startString, '');
       if (newString === '') {
         newString = `${cleanedString}`;
       } else {
         newString = `${newString}-${cleanedString}`;
-      } */
-    }
+      }
+    } */
   });
   return newString;
 }
