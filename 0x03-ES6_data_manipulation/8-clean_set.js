95045c66c8247f54c2d5eac634ee10753432d0a3
Reid-T-W <rediettadesse100@gmail.com>
2023-01-31 18:32:30 +0300

    content inside if commented out for checking purpose

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -6,12 +6,12 @@ export default function cleanSet(set, startString) {
   }
   set.forEach((x) => {
     if (x.startsWith(startString)) {
-      const cleanedString = x.replace(startString, '');
+      /* const cleanedString = x.replace(startString, '');
       if (newString === '') {
         newString = `${cleanedString}`;
       } else {
         newString = `${newString}-${cleanedString}`;
-      }
+      } */
     }
   });
   return newString;
