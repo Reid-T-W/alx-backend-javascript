b39433481e0101665891b1d4f208a89ca2b5474d
Reid-T-W <rediettadesse100@gmail.com>
2023-01-31 18:17:06 +0300

    content in if removed for checking purpose

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -6,12 +6,12 @@ export default function cleanSet(set, startString) {
   }
   set.forEach((x) => {
     if (x.includes(startString)) {
-      const cleanedString = x.replace(startString, '');
+      /* const cleanedString = x.replace(startString, '');
       if (newString === '') {
         newString = `${cleanedString}`;
       } else {
         newString = `${newString}-${cleanedString}`;
-      }
+      } */
     }
   });
   return newString;
