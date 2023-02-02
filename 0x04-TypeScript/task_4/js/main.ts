ceefed6a57664f2c31d69e0c77889505efb31120
Reid-T-W <rediettadesse100@gmail.com>
2023-02-02 16:20:42 +0300

    task10 complete

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,3 +1,33 @@
 /// <reference path="./subjects/Cpp.ts" />
 /// <reference path="./subjects/Java.ts" />
 /// <reference path="./subjects/React.ts" />
+/// <reference path="./subjects/Teacher.ts" />
+/// <reference path="./subjects/Subject.ts" />
+
+export const cpp = Subjects.Cpp;
+export const java = Subjects.Java;
+export const react = Subjects.React;
+export const cTeacher: Subjects.Teacher = {
+  firstName: 'Hanna', 
+  lastName: 'Abera', 
+  experienceTeachingC: 10
+}
+
+if (cTeacher.hasOwnProperty('experienceTeachingC')) {
+  console.log('C++');
+  const cpp1 = new cpp(cTeacher);
+  console.log(cpp1.getRequirements());
+  console.log(cpp1.getAvailableTeacher());
+}
+else if (cTeacher.hasOwnProperty('experienceTeachingJava')) {
+  console.log('Java');
+  const java1 = new java(cTeacher);
+  console.log(java1.getRequirements());
+  console.log(java1.getAvailableTeacher());
+}
+else {
+  console.log('React');
+  const react1 = new react(cTeacher);
+  console.log(react1.getRequirements());
+  console.log(react1.getAvailableTeacher());
+}
