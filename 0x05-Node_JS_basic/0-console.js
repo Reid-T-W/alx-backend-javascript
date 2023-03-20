b83e0d7ff8a936393a2420c4062abad58f9bba93
Reid-T-W <rediettadesse100@gmail.com>
2023-03-20 22:12:03 +0300

    console.log recreated

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,4 +1,6 @@
+const util = require('util');
+
 function displayMessage(msg) {
-  process.stdout.write(`${msg} \n`);
+  process.stdout.write(util.format.apply(null, [msg, '\n']));
 }
 module.exports = displayMessage;
