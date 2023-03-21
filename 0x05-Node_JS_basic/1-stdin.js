6a72e0dc66681d036653e6c9760c307ed5db78d8
Reid-T-W <rediettadesse100@gmail.com>
2023-03-21 18:16:09 +0300

    unnecessary comments removed

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -9,30 +9,3 @@ process.stdin.on('readable', () => {
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
-// const { spawn } = require('child_process');
-
-// const childProcess = spawn('node', ['1-stdin.js']);
-// childProcess.stdin.on('readable', () => {
-//   const userInput = childProcess.stdin.read();
-//   childProcess.stdout.write(`Your name is: ${userInput}\n`);
-// });
-
-// childProcess.stdin.on('end', () => {
-//   childProcess.stdout.write('This important software is now closing\n');
-// });
-
-// const { spawn } = require('child_process');
-
-// const ls = spawn('node', ['1-stdin.js']);
-
-// ls.stdout.on('data', (data) => {
-//   console.log(`stdout: ${data}`);
-// });
-
-// ls.stderr.on('data', (data) => {
-//   console.error(`stderr: ${data}`);
-// });
-
-// ls.on('close', (code) => {
-//   console.log(`child process exited with code ${code}`);
-// });
