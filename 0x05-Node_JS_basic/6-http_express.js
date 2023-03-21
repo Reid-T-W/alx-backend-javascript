d1b5606b83d937afb5a9e4b51c2ec66bb1b7094b
Reid-T-W <rediettadesse100@gmail.com>
2023-03-21 22:38:16 +0300

    eslint compliant

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -8,6 +8,7 @@ app.get('/*', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
+  console.log(`App listening on port ${port}`);
 });
+
 module.exports = app;
