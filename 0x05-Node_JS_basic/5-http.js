536143c0d8a7f8caaf98306c70b093f8e8ce9e07
Reid-T-W <rediettadesse100@gmail.com>
2023-03-23 09:59:26 +0300

    unexisting file properly handled

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -10,37 +10,42 @@ const app = http.createServer((req, res) => {
     res.end();
   } else if (req.url === '/students') {
     const path = process.argv[2];
-    res.write('This is the list of our students\n');
-    fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
-      if (err !== null) {
-        throw Error('Cannot load the database');
-      }
-      const fields = new Map();
-      const students = data.split('\n');
-      const columns = students.map((student) => student.split(','));
-      let filterdStudents = columns.filter((student) => student[3] !== undefined);
-      // Removing the header
-      filterdStudents = filterdStudents.slice(1);
-      // Filtering by field, using a hash map
-      for (const student of filterdStudents) {
-        if (!fields.has(student[3])) {
-          fields.set(student[3], [student[0]]);
-        } else {
-          fields.get(student[3]).push(student[0]);
+    try {
+      fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
+        if (err !== null) {
+          throw Error('Cannot load the database');
         }
-      }
-      res.write(`Number of students: ${filterdStudents.length}\n`);
-      // Formating the output
-      for (const key of fields.keys()) {
-        const value = fields.get(key);
-        let printValue = '';
-        for (const val of value) {
-          printValue += `${val}, `;
+        const fields = new Map();
+        const students = data.split('\n');
+        const columns = students.map((student) => student.split(','));
+        let filterdStudents = columns.filter((student) => student[3] !== undefined);
+        // Removing the header
+        filterdStudents = filterdStudents.slice(1);
+        // Filtering by field, using a hash map
+        for (const student of filterdStudents) {
+          if (!fields.has(student[3])) {
+            fields.set(student[3], [student[0]]);
+          } else {
+            fields.get(student[3]).push(student[0]);
+          }
         }
-        res.write(`Number of students in ${key}: ${value.length}. List: ${printValue.slice(0, -2)}\n`);
-      }
+        res.write('This is the list of our students\n');
+        res.write(`Number of students: ${filterdStudents.length}\n`);
+        // Formating the output
+        for (const key of fields.keys()) {
+          const value = fields.get(key);
+          let printValue = '';
+          for (const val of value) {
+            printValue += `${val}, `;
+          }
+          res.write(`Number of students in ${key}: ${value.length}. List: ${printValue.slice(0, -2)}\n`);
+        }
+        res.end();
+      });
+    } catch (error) {
+      res.write(error);
       res.end();
-    });
+    }
   }
 });
 
