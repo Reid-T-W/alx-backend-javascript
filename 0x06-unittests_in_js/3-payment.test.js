3f08b10738fe8e60f2da61ac28a91cdb5b79c486
Reid-T-W <rediettadesse100@gmail.com>
2023-03-24 09:01:45 +0300

    checking that calculateNumber is called at least once

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,12 +1,26 @@
 const sinon = require('sinon');
 const Utils = require('./utils');
+const assert = require('assert')
 const sendPaymentRequestToApi = require('./3-payment');
 
 
-describe('Testing sendPaymentRequestToApi', function() {
+describe('Testing sendPaymentRequestToApi', 
+    function() {
+        var spy_calculateNumber;
+        beforeEach(function() {
+            spy_calculateNumber = sinon.spy(Utils, 'calculateNumber');
+        })
+        afterEach(function() {
+            spy_calculateNumber.restore()
+        })
     it('spies on calculateNumber, checking arguments', function() {
-        var spy_calculateNumber = sinon.spy(Utils.calculateNumber);
         sendPaymentRequestToApi(100, 200)
         spy_calculateNumber.calledWith(100, 200)
+        assert(spy_calculateNumber.calledOnce)
+    })
+    it('checking if the calculateNumber Function is called once', function() {
+        // var spy_calculateNumber = sinon.spy(Utils, 'calculateNumber');
+        sendPaymentRequestToApi(100, 200)
+        assert(spy_calculateNumber.calledOnce)
     })
 })
