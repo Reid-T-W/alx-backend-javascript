11502aab0d22457b01aa630ad30397dac5ea1eb9
Reid-T-W <rediettadesse100@gmail.com>
2023-03-24 09:57:23 +0300

    stubbing the calculateNumber method

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,31 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const assert = require('assert')
+const sendPaymentRequestToApi = require('./4-payment');
+
+
+describe('Testing sendPaymentRequestToApi', 
+    function() {
+        var stub_calculateNumber;
+        var spy_consoleLog;
+        beforeEach(function() {
+            stub_calculateNumber = sinon.stub(Utils, 'calculateNumber');
+            // stub_calculateNumber.withArgs('SUM', 100, 20).returns(10);
+            stub_calculateNumber.returns(10);
+            spy_consoleLog = sinon.spy(console, 'log');
+        })
+        afterEach(function() {
+            spy_consoleLog.restore()
+            stub_calculateNumber.restore()
+        })
+    it('checking if the stub is called with the right args', function() {
+        // stub_calculateNumber.withArgs('SUM', 100, 20).returns(10);
+        sendPaymentRequestToApi(100, 20)
+        assert(stub_calculateNumber.calledWith('SUM', 100, 20))
+    })
+    it('checking if the logging is correct', function() {
+        // var spy_calculateNumber = sinon.spy(Utils, 'calculateNumber');
+        sendPaymentRequestToApi(100, 20)
+        assert(spy_consoleLog.calledWith('The total is: 10'))
+    })
+})
